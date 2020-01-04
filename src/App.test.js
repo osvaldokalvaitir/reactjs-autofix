400a544b3e09457e43d4574288bce0e03632bebe
Osvaldo Kalvaitir Filho <osvaldokalvaitir@outlook.com>
2020-01-04 11:55:53 -0300

    Conclusão

--- src/App.test.js
+++ src/App.test.js
@@ -6,4 +6,6 @@ it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+  expect(true).toBe(true);
+  // expect(false).toBe(true);
 });
